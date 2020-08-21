221134a6be3493a786234d15092c52aa2664522e
CatMan <garfield_lv@163.com>
2020-08-21 18:01:33 +0800

    2020年 08月 21日 星期五 18:01:33 CST

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+thisdir=$(cd $(dirname $0); pwd)
+echo $thisdir
+declare -A profiles
+profiles=(
+    [nvim]="$HOME/.config/nvim/init.vim"
+    [tmux]="$HOME/.tmux.conf"
+)
+
+for pf in $(echo ${!profiles[*]})
+do
+    echo "正在备份$pf配置文件 : ${profiles[$pf]} --》$thisdir"
+    cp ${profiles[$pf]} $thisdir
+done
+
+git add -A
+
+git commit -m "$(date)"
+
+git push -u origin
